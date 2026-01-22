7719114f68b23f608987231ee6bd317a1b1237ce
Michelle R <michelle.rich@ensign.edu>
2026-01-22 08:09:18 -0700

    completed assignment

--- patientSignUp.py
+++ patientSignUp.py
@@ -1,1 +1,5 @@
 # TODO: Build a program that will ask the user for their name and age.
+print("Welcome to VitalLink! Let's get you signed up for our healthcare services.")
+name = input("Please enter your full name: ")
+age = input("Please enter your age: ")
+print("Thank you for signing up! We have created a new record for ", name, ", age", age)
